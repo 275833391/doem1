7c84c6cbe2964d5f12b172476fa22339e9e1da02
275833391 <734314304@qq.com>
2019-08-11 23:18:32 +0800

    Create login.vue

---
+++ src/view/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container login">
+      <form>
+        <div class="form-group">
+          <label for="">用户名</label>
+          <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+          <small id="helpId" class="text-muted">Help text</small>
+        </div>
+        <div class="form-group">
+          <la for="">密码</la bel>
+          <input type="password" class="form-control" name="" id="" placeholder="">
+        </div>
+        <button type="submit" class="btn btn-primary">登录</button>
+      </form>
+    </div>
+</template>
+<style scoped>
+    .login {
+        margin-top: 50px
+    }
+</style>
